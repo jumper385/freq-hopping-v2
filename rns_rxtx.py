8ab38fc71ab9c7208fe42f3edaa4f026146ee963
jumper385 <jumper385@email.com>
2026-05-24 21:26:15 +0800

    feat: added reticulum rxtx

---
+++ rns_rxtx.py
@@ -0,0 +1,33 @@
+import argparse
+import sys
+import RNS
+
+from src.reticulum.RNSComms import RNSComms
+
+
+parser = argparse.ArgumentParser(
+        description="Minimal Impl"
+)
+
+parser.add_argument("--config",
+                    action="store",
+                    default=None,
+                    help="path to alternative Reticulum config directory",
+                    type=str)
+
+args = parser.parse_args()
+
+if args.config:
+    configarg = args.config
+else:
+    configarg = None
+
+rns_comms = RNSComms(configarg)
+rns_comms.dest_setup()
+rns_comms.announce()
+
+while True:
+    _ = input()
+    rns_comms.announce()
+    for dest_hash in list(rns_comms.peers.keys()):
+        rns_comms.send(dest_hash, "HELLO WORLD")
